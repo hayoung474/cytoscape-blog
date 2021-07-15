5735e5d74e72a49d57c7eb0b17f70b7093596375
gunwoongPark <olympiodoros@naver.com>
2021-07-15 18:54:42 +0900

    layout 결정

--- src/App.js
+++ src/App.js
@@ -13,62 +13,83 @@ function App() {
   // id속성에는 공백이나 .이 들어가면
   const [elements, setElements] = useState({
     nodes: [
-      {
-        data: { id: "one", label: "Naver", url: "https://www.naver.com/" },
-        position: { x: 100, y: 100 },
-      },
-      {
-        data: {
-          id: "two",
-          label: "Daum",
-          url: "https://www.daum.net/?nil_profile=daum&nil_src=search",
-        },
-        position: { x: 200, y: 200 },
-      },
-      {
-        data: {
-          id: "three",
-          label: "Kumoh",
-          url: "https://www.kumoh.ac.kr/ko/index.do",
-        },
-        position: { x: 300, y: 300 },
-      },
-      {
-        data: {
-          id: "four",
-          label: "Programmers",
-          url: "https://programmers.co.kr/",
-        },
-        position: { x: 400, y: 400 },
-      },
+      { data: { id: "1", label: "IP 1", type: "ip" } },
+      { data: { id: "2", label: "Device 1", type: "device" } },
+      { data: { id: "3", label: "IP 2", type: "ip" } },
+      { data: { id: "4", label: "Device 2", type: "device" } },
+      { data: { id: "5", label: "Device 3", type: "device" } },
+      { data: { id: "6", label: "IP 3", type: "ip" } },
+      { data: { id: "7", label: "Device 5", type: "device" } },
+      { data: { id: "8", label: "Device 6", type: "device" } },
+      { data: { id: "9", label: "Device 7", type: "device" } },
+      { data: { id: "10", label: "Device 8", type: "device" } },
+      { data: { id: "11", label: "Device 9", type: "device" } },
+      { data: { id: "12", label: "IP 3", type: "ip" } },
+      { data: { id: "13", label: "Device 10", type: "device" } },
     ],
-
     edges: [
       {
-        data: {
-          source: "one",
-          target: "two",
-          label: "Edge from Node1 to Node2",
-        },
+        data: { source: "1", target: "2", label: "Node2" },
       },
-
       {
-        data: {
-          source: "three",
-          target: "four",
-          label: "Edge from Node3 to Node4",
-        },
+        data: { source: "3", target: "4", label: "Node4" },
+      },
+      {
+        data: { source: "3", target: "5", label: "Node5" },
+      },
+      {
+        data: { source: "6", target: "5", label: " 6 -> 5" },
+      },
+      {
+        data: { source: "6", target: "7", label: " 6 -> 7" },
+      },
+      {
+        data: { source: "6", target: "8", label: " 6 -> 8" },
+      },
+      {
+        data: { source: "6", target: "9", label: " 6 -> 9" },
+      },
+      {
+        data: { source: "3", target: "13", label: " 3 -> 13" },
       },
     ],
   });
 
   const layout = {
-    name: "breadthfirst",
-    fit: true,
-    directed: true,
-    padding: 50,
-    avoidOverlap: true,
-    nodeDimensionsIncludeLabels: false,
+    name: "concentric",
+
+    fit: true, // whether to fit the viewport to the graph
+    padding: 30, // the padding on fit
+    startAngle: (3 / 2) * Math.PI, // where nodes start in radians
+    sweep: undefined, // how many radians should be between the first and last node (defaults to full circle)
+    clockwise: true, // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false)
+    equidistant: false, // whether levels have an equal radial distance betwen them, may cause bounding box overflow
+    minNodeSpacing: 10, // min spacing between outside of nodes (used for radius adjustment)
+    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
+    nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
+    height: undefined, // height of layout area (overrides container height)
+    width: undefined, // width of layout area (overrides container width)
+    spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
+    concentric: function (node) {
+      // returns numeric value for each node, placing higher nodes in levels towards the centre
+      return node.degree();
+    },
+    levelWidth: function (nodes) {
+      // the variation of concentric values in each level
+      return nodes.maxDegree() / 4;
+    },
+    animate: false, // whether to transition the node positions
+    animationDuration: 500, // duration of animation in ms if enabled
+    animationEasing: undefined, // easing of animation if enabled
+    animateFilter: function (node, i) {
+      return true;
+    }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
+    ready: undefined, // callback on layoutready
+    stop: undefined, // callback on layoutstop
+    transform: function (node, position) {
+      return position;
+    }, // transform a given node position. Useful for changing flow direction in discrete layouts
   };
 
   return (
@@ -76,6 +97,7 @@ function App() {
       <CytoscapeComponent
         elements={CytoscapeComponent.normalizeElements(elements)}
         zoomingEnabled={true}
+        // boxSelectionEnabled={false}
         maxZoom={2}
         minZoom={0.25}
         style={{ width: "100vh", height: "100vh" }}

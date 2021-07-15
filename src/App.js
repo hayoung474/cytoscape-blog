a2ad94b96889e0e581905b04b62b2380d408f295
gunwoongPark <olympiodoros@naver.com>
2021-07-15 13:10:39 +0900

    라이브러리 설치 및 테스트

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CytoscapeComponent from "react-cytoscapejs";
+import styled from "styled-components";
+
+const CanvasContainer = styled.div`
+  position: fixed;
+  border: 1px solid black;
+  width: 600px;
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 function App() {
+  const [elements, setElements] = useState([
+    { data: { id: "one", label: "Node 1" }, position: { x: 100, y: 100 } },
+    { data: { id: "two", label: "Node 2" }, position: { x: 200, y: 200 } },
+    {
+      data: { source: "one", target: "two", label: "Edge from Node1 to Node2" },
+    },
+    { data: { id: "three", label: "Node 3" }, position: { x: 300, y: 300 } },
+    { data: { id: "four", label: "Node 4" }, position: { x: 400, y: 400 } },
+    {
+      data: {
+        source: "three",
+        target: "four",
+        label: "Edge from Node3 to Node4",
+      },
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CanvasContainer>
+        <CytoscapeComponent
+          elements={elements}
+          style={{ width: "100%", height: "600px" }}
+        />
+      </CanvasContainer>
+    </>
   );
 }
 

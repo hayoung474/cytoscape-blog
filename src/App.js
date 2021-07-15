d8b5b59b677043309ca23035b3c5e2b1dfc9306d
gunwoongPark <olympiodoros@naver.com>
2021-07-15 19:30:59 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -61,35 +61,11 @@ function App() {
     fit: true, // whether to fit the viewport to the graph
     padding: 30, // the padding on fit
     startAngle: (3 / 2) * Math.PI, // where nodes start in radians
-    sweep: undefined, // how many radians should be between the first and last node (defaults to full circle)
     clockwise: true, // whether the layout should go clockwise (true) or counterclockwise/anticlockwise (false)
     equidistant: false, // whether levels have an equal radial distance betwen them, may cause bounding box overflow
-    minNodeSpacing: 10, // min spacing between outside of nodes (used for radius adjustment)
-    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    minNodeSpacing: 50, // min spacing between outside of nodes (used for radius adjustment)
     avoidOverlap: true, // prevents node overlap, may overflow boundingBox if not enough space
     nodeDimensionsIncludeLabels: false, // Excludes the label when calculating node bounding boxes for the layout algorithm
-    height: undefined, // height of layout area (overrides container height)
-    width: undefined, // width of layout area (overrides container width)
-    spacingFactor: undefined, // Applies a multiplicative factor (>0) to expand or compress the overall area that the nodes take up
-    concentric: function (node) {
-      // returns numeric value for each node, placing higher nodes in levels towards the centre
-      return node.degree();
-    },
-    levelWidth: function (nodes) {
-      // the variation of concentric values in each level
-      return nodes.maxDegree() / 4;
-    },
-    animate: false, // whether to transition the node positions
-    animationDuration: 500, // duration of animation in ms if enabled
-    animationEasing: undefined, // easing of animation if enabled
-    animateFilter: function (node, i) {
-      return true;
-    }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
-    ready: undefined, // callback on layoutready
-    stop: undefined, // callback on layoutstop
-    transform: function (node, position) {
-      return position;
-    }, // transform a given node position. Useful for changing flow direction in discrete layouts
   };
 
   return (
@@ -97,9 +73,46 @@ function App() {
       <CytoscapeComponent
         elements={CytoscapeComponent.normalizeElements(elements)}
         zoomingEnabled={true}
-        // boxSelectionEnabled={false}
-        maxZoom={2}
-        minZoom={0.25}
+        stylesheet={[
+          {
+            selector: "node",
+            style: {
+              backgroundColor: "#4a56a6",
+              width: 30,
+              height: 30,
+              label: "data(label)",
+            },
+          },
+          {
+            selector: "node:selected",
+            style: {
+              "border-width": "6px",
+              "border-color": "#AAD8FF",
+              "border-opacity": "0.5",
+              "background-color": "#77828C",
+              width: 50,
+              height: 50,
+              "text-outline-color": "#77828C",
+              "text-outline-width": 8,
+            },
+          },
+          {
+            selector: "node[type='device']",
+            style: {
+              shape: "rectangle",
+            },
+          },
+          {
+            selector: "edge",
+            style: {
+              width: 3,
+              "line-color": "#AAD8FF",
+              "target-arrow-color": "#6774cb",
+              "target-arrow-shape": "triangle",
+              "curve-style": "bezier",
+            },
+          },
+        ]}
         style={{ width: "100vh", height: "100vh" }}
         layout={layout}
       />

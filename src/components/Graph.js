3c3a24e16ce62f44d6e6ff8db2a124fc1ab6f391
gunwoongPark <olympiodoros@naver.com>
2021-07-18 19:29:29 +0900

    완료 초기 애니메이션만 하면 됨

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -13,6 +13,36 @@ const CustomCytoscapeComponent = styled(CytoscapeComponent)`
 function Graph({ graph }) {
   const layout = {
     name: "cose",
+    ready: function () {},
+    stop: function () {},
+    animate: true,
+    animationEasing: undefined,
+    animationDuration: 1000,
+    avoidOverlap: true,
+    animateFilter: function (node, i) {
+      return true;
+    },
+    animationThreshold: 250,
+    refresh: 20,
+    fit: true,
+    padding: 30,
+    boundingBox: undefined,
+    nodeDimensionsIncludeLabels: false,
+    randomize: false,
+    componentSpacing: 40,
+    nodeRepulsion: function (node) {
+      return 2048;
+    },
+    nodeOverlap: 4,
+    edgeElasticity: function (edge) {
+      return 32;
+    },
+    nestingFactor: 1.2,
+    gravity: 1,
+    numIter: 1000,
+    initialTemp: 1000,
+    coolingFactor: 0.99,
+    minTemp: 1.0,
   };
 
   const cyForRank = Cytoscape({

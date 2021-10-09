f8f0576b2dc811c664e08b1c02fd59a2b3790d50
parkGunWoong <bung1438@gmail.com>
2021-10-10 05:54:36 +0900

    리덕스 추가

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import admin from './admin';
 import modal from './modal';
 import graph from './graph';
+import infoModal from './infoModal';
 
-const rootReducer = combineReducers({ admin, modal, graph });
+const rootReducer = combineReducers({ admin, modal, graph, infoModal });
 
 export default rootReducer;

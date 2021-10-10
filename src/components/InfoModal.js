94651b04df867d9ab4ff38bbe976b78eeb82c4d2
parkGunWoong <bung1438@gmail.com>
2021-10-10 18:43:03 +0900

    모달 틀 잡기

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { GrClose } from 'react-icons/gr';
 
 function InfoModal({ closeInfoModal }) {
+  const userInfo = `주인 소개주인 소개주인 소개주인 소개주인 소개주인 소개주인 소개주인 소개주인 소개주인 소개
+  주인 소개주인 소개주인 소개주인 소개주인 소개
+  주인 소개주인 소개주인 소개주인 소개주인 소개`;
   return (
     <>
-      <ModalContainer></ModalContainer>
+      <ModalContainer>
+        <CustomGrClose onClick={closeInfoModal} />
+
+        <Container>
+          <ProfileImgContainer />
+          <UserName>SinaKim</UserName>
+          <UserInfo>{userInfo}</UserInfo>
+        </Container>
+
+        <Container></Container>
+      </ModalContainer>
+
       <Dim onClick={closeInfoModal} />
     </>
   );
@@ -13,13 +28,15 @@ function InfoModal({ closeInfoModal }) {
 const ModalContainer = styled.div`
   background: white;
   position: fixed;
-  width: 80%;
-  height: 75%;
+  width: 1024px;
+  height: 768px;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   border-radius: 28px;
   z-index: 1;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Dim = styled.div`
@@ -29,6 +46,43 @@ const Dim = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
+`;
+
+const CustomGrClose = styled(GrClose)`
+  font-size: 32px;
+  position: absolute;
+  top: 1%;
+  right: 1%;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProfileImgContainer = styled.div`
+  margin-top: 60px;
+  width: 360px;
+  height: 360px;
+  background-image: url('https://raw.githubusercontent.com/sina-Kim/sina-Kim.github.io/master/assets/images/sina-bear.jpg?token=APFSJOF5PUIX7EBFA4SASKLBNPOWS');
+  background-size: contain;
+  background-repeat: no-repeat;
+`;
+
+const UserName = styled.p`
+  margin: 1rem 0 0 0;
+  font-size: 32px;
+  font-weight: 700;
+  text-align: center;
+`;
+
+const UserInfo = styled.p`
+  margin: 2rem 2rem 2rem 2rem;
+  font-size: 20px;
+  white-space: pre-line;
 `;
 
 export default InfoModal;
